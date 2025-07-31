87cb5365dda558e352f3592b2b3b33e7e7cb68bc
sameer <sameerthsk@gmail.com>
2025-07-31 18:50:15 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React,{useState} from 'react'
+import { Typography, Box, styled, Toolbar, AppBar, InputBase, Badge, Avatar, Menu, MenuItem } from "@mui/material"
+import MailIcon from '@mui/icons-material/Mail';
+// import { borderRadius } from "@mui/system";
+import { FacebookRounded, Notifications } from '@mui/icons-material';
+
+const StyledToolbar = styled(Toolbar)({
+   display: "flex",
+   justifyContent: "space-between" ,
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  position: 'relative',
+  display: 'none',
+  alignItems: "center" ,
+  gap: "20px",
+  [theme.breakpoints.up("sm")] : {
+    display: "flex"
+  }
+  }));
+
+  const UserBox = styled(Box)(({ theme }) => ({
+    position: 'relative',
+    display: 'flex',
+    alignItems: "center" ,
+    gap: "10px",
+    [theme.breakpoints.up("sm")] : {
+      display: "none"
+    }
+    }));
+
+const Navbar = () => {
+  const [open,setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography  variant="h6" sx={{display:{xs:"none", sm:"block"}}}>Link</Typography>
+        <FacebookRounded sx={{display:{xs:"block", sm:"none"}}} />
+        <Search><InputBase placeholder="search"/></Search>
+        <Icons>
+            <Badge badgeContent={4} color="error">
+               <MailIcon  />
+            </Badge>
+            <Badge badgeContent={4} color="error">
+               <Notifications />
+            </Badge>
+            <Avatar sx={{width: 30 , height: 30}} 
+              src="/static/images/avatar/2.jpg"
+              onClick={(e)=> setOpen(true)} />
+            
+          </Icons>
+          <UserBox onClick={(e)=> setOpen(true)}>
+             <Avatar sx={{width: 30 , height: 30}} 
+              src="/static/images/avatar/2.jpg" />
+              <Typography variant='span'>Sam</Typography>
+          </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  )
+}
+
+export default Navbar
